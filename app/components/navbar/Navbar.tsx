d34632b273ff7f603ddf90b779aa170f5792e086
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-06-12 18:17:11 +0545

    offset-66

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,15 +1,43 @@
+'use client';
+
 import { SafeUser } from '@/app/types';
 import Container from '../Container';
 import Logo from './Logo';
 import UserMenu from './UserMenu';
+import { useEffect, useState } from 'react';
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
 }
 
+const TOP_OFFSET = 66;
+
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log(window.scrollY);
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
+    <div
+      className={`fixed w-full bg-white z-10 shadow-sm ${
+        showBackground ? 'bg-zinc-200 bg-opacity-90 transition-all' : ''
+      }`}
+    >
       <div
         className="
           py-4 
