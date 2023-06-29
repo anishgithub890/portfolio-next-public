e2ae576f47e0d6c25bc1d302ab659895643f894e
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-06-29 18:56:57 +0545

    particlecontainer-update-01

--- app/components/ParticlesContainer.tsx
+++ app/components/ParticlesContainer.tsx
@@ -1,9 +1,30 @@
+import React, { useCallback } from 'react';
+import { Particles } from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+
 const ParticlesContainer = () => {
+  // int
+
+  const particlesInit = useCallback(async (engine: any) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async () => {}, []);
   return (
-    <div>
-      <div></div>
-    </div>
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: { enable: false },
+        background: {
+          color: {
+            value: '',
+          },
+        },
+      }}
+    />
   );
 };
-
+1;
 export default ParticlesContainer;
