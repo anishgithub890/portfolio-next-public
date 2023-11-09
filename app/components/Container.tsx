e13ac8f2ab2fdab87785f0ca1da7a38764096549
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-11-09 14:48:41 +0545

    update-schema:userId

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -17,8 +17,6 @@ const Container: React.FC<ContainerProps> = ({ children }) => {
       "
     >
       {children}
-      {/* particles */}
-      {/* <ParticlesContainer /> */}
     </div>
   );
 };
