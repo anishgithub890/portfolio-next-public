74e8e1e759454432f951307b74f6668ca8469d20
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2024-01-03 18:47:42 +0545

    dashboard-map-function-added

--- app/dashboard/DashboardCard.tsx
+++ app/dashboard/DashboardCard.tsx
@@ -1,51 +1,46 @@
 'use client';
+
 import React from 'react';
 import Button from '../components/Button';
 import { useRouter } from 'next/navigation';
 
 const DashboardCard = () => {
   const router = useRouter();
+  const MNG = [
+    {
+      label: 'Manage Project',
+      link: '/projectlist',
+    },
+    {
+      label: 'Manage Skill',
+      link: '/skilllist',
+    },
+    {
+      label: 'Manage Experience',
+      link: '/experiencelist',
+    },
+    {
+      label: 'Manage Testimonial',
+      link: '/testimoniallist',
+    },
+    {
+      label: 'Read and Delete Contact',
+      link: '/contactlist',
+    },
+    {
+      label: 'Read feedback',
+      link: '/feedbacklist',
+    },
+  ];
   return (
     <>
-      <figure className="md:flex bg-slate-100 hover:bg-slate-50 transition rounded-xl p-8 md:p-8">
-        <Button
-          onClick={() => router.push('/projectlist')}
-          label="Manage Project"
-        />
-      </figure>
-      <figure className="md:flex bg-slate-100 hover:bg-slate-50 transition rounded-xl p-8 md:p-8">
-        <Button
-          onClick={() => router.push('/skilllist')}
-          label="Manage Skill"
-        />
-      </figure>
-      <figure className="md:flex bg-slate-100 hover:bg-slate-50 transition rounded-xl p-8 md:p-8">
-        <Button
-          onClick={() => router.push('/experiencelist')}
-          label="Manage Experience"
-        />
-      </figure>
-      <figure className="md:flex bg-slate-100 hover:bg-slate-50 transition rounded-xl p-8 md:p-8">
-        <Button
-          onClick={() => router.push('/testimoniallist')}
-          label="Manage Testimonial"
-        />
-      </figure>
-      <figure className="md:flex bg-slate-100 hover:bg-slate-50 transition rounded-xl p-8 md:p-8">
-        <Button
-          onClick={() => router.push('/contactlist')}
-          label="Read and Delete Contact"
-        />
-      </figure>
-      <figure className="md:flex bg-slate-100 hover:bg-slate-50 transition rounded-xl p-8 md:p-8">
-        <Button
-          onClick={() => router.push('/feedbacklist')}
-          label="Read your feedback"
-        />
-      </figure>
-      <figure className="md:flex bg-slate-100 hover:bg-slate-50 transition rounded-xl p-8 md:p-8">
-        <Button onClick={() => {}} label="User" />
-      </figure>
+      {MNG.map(({ label, link }, key) => (
+        <div key={key}>
+          <figure className="md:flex bg-slate-100 hover:bg-slate-50 transition rounded-xl p-8 md:p-8">
+            <Button onClick={() => router.push(link)} label={label} />
+          </figure>
+        </div>
+      ))}
     </>
   );
 };
